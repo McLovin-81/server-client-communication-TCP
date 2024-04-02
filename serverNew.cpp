7b37c63a1c734e0069e50d51c1e13f20c3930197
Felipe <cristian.castillo.barrero@deutsche-boerse.com>
2024-04-02 10:35:21 +0200

    some comments for serverNew.cpp

--- serverNew.cpp
+++ serverNew.cpp
@@ -2,21 +2,23 @@
 #include <cstring>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <sys/socket.h>
+#include <sys/socket.h> // Do i need this?
 
 int main() {
     // Create socket
-    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0); // Replace 0 with IPPROTO_TCP
     if (server_socket == -1) {
         std::cerr << "Error: Could not create socket\n";
         return 1;
     }
 
-    // Bind socket
-    sockaddr_in server_address;
+    // Prepare server address structure
+    sockaddr_in server_address; // Add struct
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(54000);  // Port number
     server_address.sin_addr.s_addr = INADDR_ANY;
+
+    // Bind socket
     if (bind(server_socket, reinterpret_cast<sockaddr*>(&server_address), sizeof(server_address)) == -1) {
         std::cerr << "Error: Could not bind socket to IP/Port\n";
         return 2;
@@ -39,6 +41,7 @@ int main() {
     char buffer[4096];
     while (true) {
         memset(buffer, 0, sizeof(buffer));
+
         int bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
         if (bytes_received == -1) {
             std::cerr << "Error: Could not receive message from client\n";
@@ -47,6 +50,7 @@ int main() {
             std::cout << "Client disconnected\n";
             break;
         }
+        
         std::cout << "Client: " << std::string(buffer, 0, bytes_received) << std::endl;
     }
 
